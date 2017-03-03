20facff0c9b0e96db28c35158ed9691b3111353d
damienbod <damien_bod@hotmail.com>
2017-03-03 09:40:53 +0100

    Logging to MySQL

---
+++ src/ConsoleNLogMySQL/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Targets;
+
+namespace ConsoleNLog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            LogManager.Configuration.Variables["configDir"] = "C:\\git\\damienbod\\AspNetCoreNlog\\Logs";
+
+            var logger = LogManager.GetLogger("console");
+            logger.Warn("console logging is great");
+
+            Console.WriteLine("log sent");
+            Console.ReadKey();
+        }
+    }
+}
