d355969711afbbd76c08e6b875e7f7d639cfa4b3
damienbod <damien_bod@hotmail.com>
2017-03-23 09:10:01 +0100

    adding init for postgresql target

---
+++ src/ConsoleNLogPostgreSQL/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NLog;
+
+namespace ConsoleNLogPostgreSQL
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            LogManager.Configuration.Variables["configDir"] = "C:\\git\\damienbod\\AspNetCoreNlog\\Logs";
+
+            var logger = LogManager.GetLogger("console");
+            logger.Warn("console logging is great");
+            logger.Error(new ArgumentException("oh no"));
+            Console.WriteLine("log sent");
+            Console.ReadKey();
+        }
+    }
+}
